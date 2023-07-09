c69b3d7e46e990523c9a853818a020994e2b6014
Leonam Knupp <leonamknupp@hotmail.com>
2023-07-09 19:43:23 -0300

    fix: change id_curso variable name to id

---
+++ backend/src/schemas/cursoSchema.py
@@ -0,0 +1,17 @@
+import pydantic as _pydantic
+
+
+class _CursoBase(_pydantic.BaseModel):
+    nome: str
+    campus: str
+
+    class Config:
+        orm_mode = True
+
+
+class CursoCreate(_CursoBase):
+    pass
+
+
+class Curso(_CursoBase):
+    id: int
