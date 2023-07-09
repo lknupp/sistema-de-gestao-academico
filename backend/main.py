ee3c28152b151ebc60f78ef6ffa398b137a2d06e
Leonam Knupp <leonamknupp@hotmail.com>
2023-07-09 19:45:33 -0300

    fix: conflict resolution in main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,12 @@ import fastapi as _fastapi
 import src.database.sqlite as _database
 from src.routes import alunoRoute as _alunoRoute
 from src.routes import enderecoRoute as _enderecoRoute
+<<<<<<< HEAD
 from src.routes import professorRoute as _profRoute
 from src.routes import telefoneRoute as _telRoute
+=======
+from src.routes import cursoRoute as _cursoRoute
+>>>>>>> 9b9777e (feat: include cursoRoute to app.router)
 
 app = _fastapi.FastAPI()
 
@@ -12,4 +16,8 @@ _database.create_database()
 app.include_router(_alunoRoute.router)
 app.include_router(_profRoute.router)
 app.include_router(_enderecoRoute.router)
+<<<<<<< HEAD
 app.include_router(_telRoute.router)
+=======
+app.include_router(_cursoRoute.router)
+>>>>>>> 9b9777e (feat: include cursoRoute to app.router)
