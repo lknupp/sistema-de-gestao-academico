c1584a0de43dab36163b74ce7233ea2e55c6a8d9
Leonam Knupp <leonamknupp@hotmail.com>
2023-07-11 20:22:06 -0300

    feat: update database file connection

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,12 +1,21 @@
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./src/database/college.db"
+DATABASE = os.getenv('DATABASE')
+USER = os.getenv('APP_USER')
+PASSWORD = os.getenv('APP_PASSWORD')
+HOST = os.getenv('DB_HOST')
+PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+SQLALCHEMY_DATABASE_URL = f'{DATABASE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}'
 
 engine = _sql.create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = _orm.sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
 Base = _declarative.declarative_base()
 
