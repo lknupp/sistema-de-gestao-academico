941361e6860ce805c1f4afa3b1d2d0740fec0df0
Matheus Loiola <matheusloiolapinto@gmail.com>
2023-07-17 21:19:46 -0300

    feat: improved historico model

--- backend/src/models/aluno.py
+++ backend/src/models/aluno.py
@@ -1,8 +1,19 @@
 import sqlalchemy as _sql
+import sqlalchemy.orm as _orm
 
 from ..database import database as _database
 from . import pessoa as _pessoa
-from . import oferta as _oferta
+
+
+rl_historico = _sql.Table(
+    "rl_historico",
+    _database.Base.metadata,
+    _sql.Column("id_historico", _sql.Integer, primary_key=True, index=True),
+    _sql.Column("nota", _sql.Float),
+    _sql.Column("faltas", _sql.Integer),
+    _sql.Column("id_aluno_fk", _sql.Integer, _sql.ForeignKey("aluno.id_pessoa")),
+    _sql.Column("id_oferta_fk", _sql.Integer, _sql.ForeignKey("oferta.id_oferta")),
+)
 
 
 class Aluno(_pessoa.Pessoa, _database.Base):
@@ -10,6 +21,6 @@ class Aluno(_pessoa.Pessoa, _database.Base):
     data_ingresso = _sql.Column(_sql.Date)
     data_nascimento = _sql.Column(_sql.Date)
 
-    ofertas = _sql.orm.relationship(
-        "Oferta", secondary=_oferta.rl_historico, back_populates="historico"
+    historico = _orm.relationship(
+        "Oferta", secondary=rl_historico, back_populates="ofertas"
     )
--- backend/src/models/oferta.py
+++ backend/src/models/oferta.py
@@ -1,16 +1,8 @@
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 from ..database import database as _database
+from . import aluno as _aluno
 
-rl_historico = _sql.Table(
-    "rl_historico",
-    _database.Base.metadata,
-    _sql.Column("id_historico", _sql.Integer, primary_key=True, index=True),
-    _sql.Column("nota", _sql.Float),
-    _sql.Column("faltas", _sql.Integer),
-    _sql.Column("id_aluno_fk", _sql.Integer, _sql.ForeignKey("aluno.id_pessoa")),
-    _sql.Column("id_oferta_fk", _sql.Integer, _sql.ForeignKey("oferta.id_oferta")),
-)
 
 
 class Oferta(_database.Base):
@@ -20,9 +12,10 @@ class Oferta(_database.Base):
     semestre = _sql.Column(_sql.Integer)
     periodo = _sql.Column(_sql.String)
 
-    historico = _orm.relationship(
-        "Aluno", secondary=rl_historico, back_populates="ofertas"
+    ofertas = _sql.orm.relationship(
+        "Aluno", secondary=_aluno.rl_historico, back_populates="historico"
     )
+
     id_disciplina = _sql.Column(
         _sql.Integer, _sql.ForeignKey("disciplina.id_disciplina")
     )
