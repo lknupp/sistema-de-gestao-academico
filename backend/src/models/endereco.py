2c5fcf8bfdb4d49760513338fc48187a2610e777
Leonam Knupp <leonamknupp@hotmail.com>
2023-07-17 23:17:06 -0300

    fix: change variable name from local to bairro

--- backend/src/models/endereco.py
+++ backend/src/models/endereco.py
@@ -9,11 +9,12 @@ class Endereco(_database.Base):
     id_endereco = _sql.Column(_sql.Integer, primary_key=True, index=True)
     logradouro = _sql.Column(_sql.String)
     numero = _sql.Column(_sql.String)
-    local = _sql.Column(_sql.String)
+    bairro = _sql.Column(_sql.String)
     cep = _sql.Column(_sql.String)
     tipo = _sql.Column(_sql.String)
 
-    aluno = _orm.relationship("Aluno", backref="endereco", cascade="all, delete")
+    aluno = _orm.relationship(
+        "Aluno", backref="endereco", cascade="all, delete")
     professor = _orm.relationship(
         "Professor", backref="endereco", cascade="all, delete"
     )
