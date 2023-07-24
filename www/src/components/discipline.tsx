137e527f1980a5060feea531b2b9d13a62c39c74
caio <hi@caio.sh>
2023-07-24 10:18:51 -0300

    added frontend without data fetching

---
+++ www/src/components/discipline.tsx
@@ -0,0 +1,133 @@
+import { Link } from "react-router-dom";
+import { Input } from "../App";
+import { useState } from "react";
+
+export function Disciplines() {
+  const disciplines = [
+    {
+      id: 1,
+      nome: 'Algortimos e Programação',
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nis',
+      id_curso: 'Ciência da Computação',
+    },
+    {
+      id: 2,
+      nome: 'Estrutura de Dados',
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nis',
+      id_curso: 'Ciência da Computação',
+    }
+  ];
+
+  if (disciplines.length === 0) return (
+    <>
+      <h1 className="text-3xl">disciplinas</h1>
+      <p className="mt-5">Nenhum disciplina encontrado.</p>
+    </>
+  )
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl">disciplinas</h1>
+        <Link to="/disciplina/create" className="p-2 mt-2 text-stone-200 w-fit bg-zinc-900 hover:bg-zinc-700">+ adicionar</Link>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-left">
+                <thead className="font-medium border-b dark:border-stone-200">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">nome</th>
+                    <th scope="col" className="px-6 py-4">curso</th>
+                    <th scope="col" className="px-6 py-4">#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {disciplines.map((discipline) => (
+                    <tr key={discipline.id} className="border-b dark:border-stone-200">
+                      <td className="px-6 py-4 whitespace-nowrap">{discipline.nome}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{discipline.id_curso}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <Link to={`/disciplina/${discipline.id}`} className="underline">detalhes</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export function Discipline() {
+  const discipline: any = {
+    id: 1,
+    nome: 'Algortimos e Programação',
+    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam nis',
+    id_curso: 'Ciência da Computação',
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-3xl">disciplina</h1>
+        <Link to="/disciplina" className="p-2 mt-2 text-stone-200 w-fit bg-zinc-900 hover:bg-zinc-700">voltar</Link>
+      </div>
+
+      <div className="flex justify-around gap-10">
+        <ul className="flex-grow">
+          {Object.keys(discipline).map((key) => (
+            <li key={key} className="w-full py-4 border-b-2 border-opacity-50 border-stone-200">
+              <strong>{key}:</strong> {discipline[key]}
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex-1">
+          <img src="https://placekitten.com/200/200" alt="Foto do disciplina" />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export function DisciplineCreate() {
+  const [discipline, setDiscipline] = useState({
+    nome: '',
+    descricao: '',
+    id_curso: ''
+  });
+
+  function handleChange(event: any) {
+    const { name, value } = event.target;
+    setDiscipline({ ...discipline, [name]: value });
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(discipline);
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-3xl">adicionar disciplina</h1>
+        <Link to="/disciplina" className="p-2 mt-2 text-stone-200 w-fit bg-zinc-900 hover:bg-zinc-700">voltar</Link>
+      </div>
+
+      <form className="grid grid-cols-2 gap-4 mt-5" onSubmit={handleSubmit}>
+        <Input label="nome" name="nome" value={discipline.nome} onChange={handleChange} />
+        <Input label="curso" name="id_curso" value={discipline.id_curso} onChange={handleChange} />
+        <Input label="descricao" name="data_ingresso" value={discipline.descricao} onChange={handleChange} />
+
+        <button className="p-2 mt-5 text-white w-min bg-zinc-900 hover:bg-zinc-700">enviar</button>
+      </form>
+    </>
+  )
+}
---
+++ www/src/main.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App, Home } from './App.tsx'
+import ErrorPage from './error-page.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import { Student, StudentCreate, Students } from './components/student.tsx'
+import { Professor, ProfessorCreate, Professors } from './components/professor.tsx'
+import { Course, CourseCreate, Courses } from './components/course.tsx'
+import { Discipline, DisciplineCreate, Disciplines } from './components/discipline.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "aluno",
+        element: <Students />,
+      },
+      {
+        path: "aluno/create",
+        element: <StudentCreate />,
+      },
+      {
+        path: "aluno/:id",
+        element: <Student />,
+      },
+      {
+        path: "professor",
+        element: <Professors />,
+      },
+      {
+        path: "professor/:id",
+        element: <Professor />,
+      },
+      {
+        path: "professor/create",
+        element: <ProfessorCreate />,
+      },
+      {
+        path: "curso",
+        element: <Courses />,
+      },
+      {
+        path: "curso/:id",
+        element: <Course />,
+      },
+      {
+        path: "curso/create",
+        element: <CourseCreate />,
+      },
+      {
+        path: "disciplina",
+        element: <Disciplines />,
+      },
+      {
+        path: "disciplina/:id",
+        element: <Discipline />,
+      },
+      {
+        path: "disciplina/create",
+        element: <DisciplineCreate />,
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
